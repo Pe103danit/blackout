1fa789a1a17671aa42c105019d8752703ca9663f
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2023-08-07 00:43:46 +0300

    fixed Project with stylelint "bugs"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Header from './pages/Header/Header';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
-  return (
-      <BrowserRouter>
-      <Header/>
-        <Routes>
-          <Route
-          path='/'
-          element={<div>1</div>}
-          />
-        </Routes>
-      </BrowserRouter>
-  )
+    return (
+        <>
+            <Header/>
+            <Routes>
+                <Route
+                    path='/'
+                    element={<div>1</div>}
+                />
+            </Routes>
+        </>
+    )
 }
 export default App;
