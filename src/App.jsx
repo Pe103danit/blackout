e47aad914e20f5f5f180a6a69866198d570c2a5b
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2023-08-06 19:10:56 +0300

    added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Header from "./pages/Header/Header";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
+      <>
       <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route
+          path=""
+          element={<div>1</div>}
+          />
+        </Routes>
+      </BrowserRouter>
+      </>
   )
 }
 
