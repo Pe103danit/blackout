cb258a554e922cb541256c2a1ebe58ae80ecba76
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2023-08-06 23:39:33 +0300

    finished Header with stylelint "bugs"

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,9 +1,9 @@
-import logoLeft from './logo-left-removebg-preview.png'
-import logoRight from './logo-right-removebg-preview.png'
-import {SearchIcon, MarketIcon, UserIcon} from '../../components/assets/Icons';
+import logoLeft from './logo-left-removebg-preview.png';
+import logoRight from './logo-right-removebg-preview.png';
+import { SearchIcon, MarketIcon, UserIcon } from '../../components/assets/Icons';
 
-import style from './Header.module.scss'
-import {NavLink} from 'react-router-dom';
+import style from './Header.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -11,10 +11,10 @@ const Header = () => {
             <div className={style.header_c1}>
                 <div className={style.header_c1_logos}>
                     <NavLink to='/' className={style.header_c1_logos_left}>
-                        <img src={logoLeft} alt='logo-left'/>
+                        <img src={logoLeft} alt='logo-left' />
                     </NavLink>
                     <button className={style.header_c1_logos_right}>
-                        <img src={logoRight} alt='logo-right' title='Click me'/>
+                        <img src={logoRight} alt='logo-right' title='Click me' />
                     </button>
                 </div>
                 <nav className={style.header_c1_navigation}>
@@ -30,17 +30,17 @@ const Header = () => {
             </div>
             <div className={style.header_c2}>
                 <button>
-                    <SearchIcon/>
+                    <SearchIcon />
                 </button>
                 <button>
-                    <UserIcon/>
+                    <UserIcon />
                 </button>
                 <button>
-                    <MarketIcon/>
+                    <MarketIcon />
                 </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
