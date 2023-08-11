1e234bff56270ebcb6004ecd45cc73dad120fcdb
Maxim Yatsenko <maximkayatsenccko9@gmail.com>
2023-08-12 01:07:53 +0300

    add adaptive and dark theme for why choose us

---
+++ src/components/WhyChooseUs/configChoose.js
@@ -0,0 +1,33 @@
+export const configChoose = [
+    {
+        src: 'https://cdn.shopify.com/s/files/1/0592/6004/3456/files/Shipping_3x.svg?v=1675165253',
+        title: 'Fast & Free Shipping',
+        text: 'Orders are usually processed and delivered within 4-10 business days.'
+    },
+    {
+        src: 'https://cdn.shopify.com/s/files/1/1996/9707/files/Warranty_3x.svg?v=1670212902',
+        title: 'Warranty Protection',
+        text: 'All products are covered by a warranty service.'
+    },
+    {
+        src: 'https://cdn.shopify.com/s/files/1/1996/9707/files/Payments_3x.svg?v=1670212902',
+        title: 'Secure Payments',
+        text: 'Pay by debit or credit card, PayPal, or other secure payment platform.'
+    },
+     {
+        src: 'https://cdn.shopify.com/s/files/1/1996/9707/files/Support_3x.svg?v=1670212902',
+        title: 'Lifetime Customer Support',
+        text: '9AM to 5PM BST (Weekdays)'
+    },
+     {
+        src: 'https://cdn.shopify.com/s/files/1/1996/9707/files/Convenient_Installment_Plans_3x.svg?v=1669967314',
+        title: 'Convenient Installment Plans',
+        text: 'We offer a range of installment options, including PayPal and Klarna.'
+    },
+     {
+        src: 'https://cdn.shopify.com/s/files/1/1996/9707/files/Global_Elite_Membership_3x.svg?v=1669967315',
+        title: 'Earn Rewards/Global Elite Membership',
+        text: 'Become a member to earn EcoCredits and exclusive rewards every time you shop.'
+    }
+
+]
