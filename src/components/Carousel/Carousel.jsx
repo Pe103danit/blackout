89f674d59928bcdc76cb1cd997cf1e5448d0052c
Yas-X <yasx80@gmail.com>
2023-08-12 10:47:10 +0300

    Update carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,7 +4,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-// import './Carousel.scss';
+import './Carousel.scss';
 import style from './Carousel.module.scss'
 
 const Carousel = () => {
