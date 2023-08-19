405d8d029371ba184e295a2122b8bf2904f1319d
Yas-X <yasx80@gmail.com>
2023-08-19 15:26:52 +0300

    Create ShopCard Component and integreate to Shop

--- src/components/Carousel/ShopCard/ShopCard.jsx
+++ src/components/Carousel/ShopCard/ShopCard.jsx
@@ -1,9 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { instance } from '../../assets/axiosUrl'
+
+import style from './ShopCard.module.scss';
+
 const ShopCard = () => {
+    const [productItems, setProductItems] = useState([]);
+
+  useEffect(() => {
+    instance.get('/api/products')
+      .then(response => {
+        setProductItems(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(productItems);
 
     return (
         <div className="shopCard">
-            <img className={style.shopCard__img} src={cardProduct.imageUrl[0]} alt={product.title} />
-            
+            {/* <img className={style.shopCard__img} src={cardProduct.imageUrl[0]} alt={product.title} /> */}
+            <div className={style.shopCard__description}>
+                <h3 className={style.shopCard__description__name}>Good name</h3>
+                <h3 className={style.shopCard__description__brand}>Good brand</h3>
+                <p className={style.shopCard__description__model}>Good model</p>
+                <h5 className={style.shopCard__description__price}>Good price</h5>
+            </div>
 
         </div>
     )
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,6 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { instance } from '../../components/assets/axiosUrl'
+
+import style from './Shop.module.scss';
+
+import ShopCard from '../../components/Carousel/ShopCard/ShopCard';
 const Shop = () => {
     return (
-        <div>Shop</div>
+        <div>
+            <ShopCard/>
+        </div>
     )
 }
 export default Shop
