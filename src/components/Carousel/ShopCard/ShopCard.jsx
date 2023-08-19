aedb16240d2b53e34fb5284c8e5b6824c25b595c
Yas-X <yasx80@gmail.com>
2023-08-19 14:56:29 +0300

    Create ShopCard Component

--- src/components/Carousel/ShopCard/ShopCard.jsx
+++ src/components/Carousel/ShopCard/ShopCard.jsx
@@ -3,6 +3,7 @@ const ShopCard = () => {
     return (
         <div className="shopCard">
             <img className={style.shopCard__img} src={cardProduct.imageUrl[0]} alt={product.title} />
+            
 
         </div>
     )
