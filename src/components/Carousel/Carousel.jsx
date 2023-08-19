cad5a02b3a56dc9c38497fe316c5e2b4e51be02e
Yas-X <yasx80@gmail.com>
2023-08-19 08:36:37 +0300

    Change navLink to all card slider

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -53,51 +53,51 @@ const Carousel = () => {
       });
   }, []);
 
-//   const products = [
-//   {
-//     customId: 'EcoFlow WAVE 2 Portable Air Conditioner',
-//     title: 'WAVE 2',
-//     description: 'The most powerful and compact portable AC',
-//     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/PC-Shopify_banner.jpg?v=1684156510',
-//     htmlContent: 'https://uk.ecoflow.com/products/wave-2-portable-air-conditioner',
-//   },
-//   {
-//     customId: 'EcoFlow DELTA 2 Max Portable Power Station',
-//     title: 'DELTA 2 Max',
-//     description: '10 Years of Everyday Power',
-//     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/banner_-_51201080_1_d52eceda-c1fc-4f8d-806b-3204a23a54dd.png?v=1685024243',
-//     htmlContent: 'https://uk.ecoflow.com/products/delta-2-max-portable-power-station',
-//   },
-//   {
-//     customId: 'EcoFlow Smart Generator (Dual Fuel)',
-//     title: 'Smart Generator (Dual Fuel)',
-//     description: 'Fuel Your Freedom.',
-//     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/DG200_3x_d361a46c-64b2-4579-a5fc-191dbe0f9eb6.png?v=1675164897',
-//     htmlContent: 'https://uk.ecoflow.com/products/dual-fuel-generator',
-//   },
-//   {
-//     customId: 'Portable RV & EV Power with DELTA Pro',
-//     title: 'Portable PV & EV Power with DELTA Pro',
-//     description: 'Travel Without Limits',
-//     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/PC.jpg?v=1680243561',
-//     htmlContent: 'https://uk.ecoflow.com/products/portable-rv-ev-power-with-delta-pro',
-//   }
-// ]
+  //   const products = [
+  //   {
+  //     customId: 'EcoFlow WAVE 2 Portable Air Conditioner',
+  //     title: 'WAVE 2',
+  //     description: 'The most powerful and compact portable AC',
+  //     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/PC-Shopify_banner.jpg?v=1684156510',
+  //     htmlContent: 'https://uk.ecoflow.com/products/wave-2-portable-air-conditioner',
+  //   },
+  //   {
+  //     customId: 'EcoFlow DELTA 2 Max Portable Power Station',
+  //     title: 'DELTA 2 Max',
+  //     description: '10 Years of Everyday Power',
+  //     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/banner_-_51201080_1_d52eceda-c1fc-4f8d-806b-3204a23a54dd.png?v=1685024243',
+  //     htmlContent: 'https://uk.ecoflow.com/products/delta-2-max-portable-power-station',
+  //   },
+  //   {
+  //     customId: 'EcoFlow Smart Generator (Dual Fuel)',
+  //     title: 'Smart Generator (Dual Fuel)',
+  //     description: 'Fuel Your Freedom.',
+  //     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/DG200_3x_d361a46c-64b2-4579-a5fc-191dbe0f9eb6.png?v=1675164897',
+  //     htmlContent: 'https://uk.ecoflow.com/products/dual-fuel-generator',
+  //   },
+  //   {
+  //     customId: 'Portable RV & EV Power with DELTA Pro',
+  //     title: 'Portable PV & EV Power with DELTA Pro',
+  //     description: 'Travel Without Limits',
+  //     imageUrl: 'https://uk.ecoflow.com/cdn/shop/files/PC.jpg?v=1680243561',
+  //     htmlContent: 'https://uk.ecoflow.com/products/portable-rv-ev-power-with-delta-pro',
+  //   }
+  // ]
 
   return (
     <div className={style.carousel}>
       <Slider {...settings}>
         {carouselItems.map((product, index) => (
-          <div key={index} className={style.carousel__card}>
-            <img className={style.carousel__card__img} src={product.imageUrl} alt={product.name} />
-            <div className={style.carousel__card__content}>
-              <h3 className={style.carousel__card__title}>{product.title}</h3>
-              <p className={style.carousel__card__text}>{product.description}</p>
-              <Link to={product.htmlContent}>
+          <Link to={product.htmlContent}>
+            <div key={index} className={style.carousel__card}>
+              <img className={style.carousel__card__img} src={product.imageUrl} alt={product.name} />
+              <div className={style.carousel__card__content}>
+                <h3 className={style.carousel__card__title}>{product.title}</h3>
+                <p className={style.carousel__card__text}>{product.description}</p>
                 <button className={style.carousel__card__button} >LEARN MORE</button>
-              </Link>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </Slider>
     </div>
