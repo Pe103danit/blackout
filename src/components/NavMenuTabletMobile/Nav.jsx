1b0bb09e4205ee54f112a3b0aec8bde0a479a405
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2023-08-23 00:26:25 +0300

    - some fixes

--- src/components/NavMenuTabletMobile/Nav.jsx
+++ src/components/NavMenuTabletMobile/Nav.jsx
@@ -1,28 +1,28 @@
 import style from './NavMenuTabletMobile.module.scss'
 import { NavLink } from 'react-router-dom'
 
-const Nav = ({themeStyle}) => {
+const Nav = ({themeStyle, toggle}) => {
 const navStyle = themeStyle === 'light' ? 'lightMobilePanel' : 'darkMobilePanel'
   return (
     <nav className={`${style.container_nav} ${navStyle}`}>
       <ul className={style.container_nav_list}>
         <li className={style.container_nav_list_item}>
-          <NavLink to={'/shop'} className={style.container_nav_list_item_link}>Shop</NavLink>
+          <NavLink to={'/shop'} className={style.container_nav_list_item_link} onClick={toggle}>Shop</NavLink>
         </li>
         <li className={style.container_nav_list_item}>
-          <NavLink to={'/offers'} className={style.container_nav_list_item_link}>Offers</NavLink>
+          <NavLink to={'/offers'} className={style.container_nav_list_item_link} onClick={toggle}>Offers</NavLink>
         </li>
         <li className={style.container_nav_list_item}>
-          <NavLink to={'/delivery'} className={style.container_nav_list_item_link}>Delivery</NavLink>
+          <NavLink to={'/delivery'} className={style.container_nav_list_item_link} onClick={toggle}>Delivery</NavLink>
         </li>
         <li className={style.container_nav_list_item}>
-          <NavLink to={'/payment'} className={style.container_nav_list_item_link}>Payment</NavLink>
+          <NavLink to={'/payment'} className={style.container_nav_list_item_link} onClick={toggle}>Payment</NavLink>
         </li>
         <li className={style.container_nav_list_item}>
-          <NavLink to={'/about_us'} className={style.container_nav_list_item_link}>About us</NavLink>
+          <NavLink to={'/about_us'} className={style.container_nav_list_item_link} onClick={toggle}>About us</NavLink>
         </li>
         <li className={style.container_nav_list_item}>
-          <NavLink to={'/contacts'} className={style.container_nav_list_item_link}>Contacts</NavLink>
+          <NavLink to={'/contacts'} className={style.container_nav_list_item_link} onClick={toggle}>Contacts</NavLink>
         </li>
       </ul>
     </nav>
