6c4ed217473f6c1899856a8ee42c1ce3cae9758a
Yas-X <yasx80@gmail.com>
2023-08-25 22:35:12 +0300

    Dark Theme for ShopCard

--- src/components/ShopCard/ShopCard.jsx
+++ src/components/ShopCard/ShopCard.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import style from './ShopCard.module.scss';
 
 const ShopCard = (props) => {
+    const theme = useSelector(state => state.UIStateReducer.lightTheme);
+
     return (
         <Link to={`/${props.productItem.itemNo}`}>
-            <div className={style.shopCard}>
+            <div className={`${style.shopCard} ${theme ? '' : style.shopCard__darkTheme}`}>
                 <div className={style.shopCard__imgBlock}>
                     <img className={style.shopCard__imgBlock__img1} src={props.productItem.imageUrls[0]} alt={props.productItem.title} />
-                    <img className={style.shopCard__imgBlock__img2} src={props.productItem.imageUrls[2]} alt={props.productItem.title} />
+                    <img className={style.shopCard__imgBlock__img2} src={props.productItem.imageUrls[1]} alt={props.productItem.title} />
                 </div>
                 <div className={style.shopCard__description}>
-                    <h3 className={style.shopCard__description__name}>{props.productItem.name}</h3>
-                    <h3 className={style.shopCard__description__categories}>{props.productItem.categories}</h3>
-                    <p className={style.shopCard__description__model}>{props.productItem.model}</p>
+                    <h3 className={`${style.shopCard__description__name} ${theme ? '' : style.shopCard__description__name__darkTheme}`}>{props.productItem.name}</h3>
+                    <h3 className={`${style.shopCard__description__categories} ${theme ? '' : style.shopCard__description__categories__darkTheme}`}>{props.productItem.categories}</h3>
+                    <p className={`${style.shopCard__description__model} ${theme ? '' : style.shopCard__description__model__darkTheme}`}>{props.productItem.model}</p>
                     <div className={style.shopCard__description__order}>
-                        <h5 className={style.shopCard__description__order__price}>${props.productItem.currentPrice} USD</h5>
+                        <h5 className={`${style.shopCard__description__order__price} ${theme ? '' : style.shopCard__description__order__price__darkTheme}`}>${props.productItem.currentPrice} USD</h5>
                         <button className={style.shopCard__description__order__btn}>SHOP NOW</button>
                     </div>
 
