7a884a3803e97fe8f243692cf6c88270c274bfd1
Max Kutsulyma <xkutsulymkax@gmail.com>
2023-08-28 22:32:58 +0300

    commit

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import style from './ProductCard.module.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import StarRating from './StarRating';
