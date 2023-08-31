8d18e97288a12c245a43787656a9ba29ab790c18
Max Kutsulyma <xkutsulymkax@gmail.com>
2023-08-31 14:55:00 +0300

    commit

--- src/pages/ProductCardPage/ProductCardPage.jsx
+++ src/pages/ProductCardPage/ProductCardPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProductCard from '../../components/ProductCard/ProductCard'
-import { useSelector } from 'react-redux'
+
 
 const ProductCardPage = () => {
   // const productIsLoading = useSelector(state => state.ProductReducer.productIsLoading)
