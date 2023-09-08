a05d7394a8ae22745d085f7725e22c824c34891e
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2023-09-08 13:54:43 +0300

    - added country flags for phone number

--- src/pages/Order/InformationStep1/InformationStep1.jsx
+++ src/pages/Order/InformationStep1/InformationStep1.jsx
@@ -2,8 +2,9 @@ import style from './InformationStep1.module.scss'
 import { NavLink } from 'react-router-dom'
 import { MarketIcon } from '../../../components/assets/Icons'
 import { useFormik } from 'formik'
-/* import {PatternFormat} from 'react-number-format';
-import * as Yup from 'yup'; */
+import * as Yup from 'yup';
+import ReactPhoneInput from 'react-phone-input-material-ui';
+import {findCurrentCountry} from './ListCountries'
 import {
   Button,
   Checkbox,
@@ -14,6 +15,7 @@ import {
   NativeSelect,
   TextField
 } from '@mui/material'
+import { useState } from 'react'
 
 const InformationStep1 = (props) => {
   const themeStyle = props.lightTheme
@@ -29,10 +31,33 @@ const InformationStep1 = (props) => {
       email: ''
     },
     onSubmit: values => {
-      console.log(values);
-    }
+      console.log({...values, phone: phoneInput});
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .min(1, 'There should be more characters')
+        .max(50, 'There should be less characters')
+        .required('Write please your First Name'),
+      lastName: Yup.string()
+        .min(1, 'There should be more characters')
+        .max(50, 'There should be less characters')
+        .required('Write please your Last Name'),
+      email: Yup.string().email()
+        .min(5, 'There should be more characters')
+        .max(50, 'There should be less characters')
+        .required('Write please your Email'),
+      phone: Yup.string()
+        .required('Write please your Phone number')
+    })
   })
+  const [phoneInput, setPhoneInput] = useState('')
+  const [countryInfo, setCountryInfo] = useState(null)
+  const handleSetPhone = (e) => {
+    setPhoneInput(e)
+    setCountryInfo(findCurrentCountry(e))
+  }
   return (
+
     <div className={`${style.container} ${themeStyle}`}>
       <div className={style.container_title}>
         <div className={style.container_title_inner}>
@@ -65,7 +90,11 @@ const InformationStep1 = (props) => {
             </li>
           </ul>
         </nav>
-        <form className={style.container_main_form} onSubmit={formik.handleSubmit}>
+        <form className={style.container_main_form}
+              onSubmit={formik.handleSubmit}
+              autoComplete="off"
+              noValidate
+        >
           <div className={style.container_main_form_login}>
             <p className={style.container_main_form_login_title}>Contact</p>
             <p className={style.container_main_form_login_question}>Have an account?
@@ -86,6 +115,7 @@ const InformationStep1 = (props) => {
                   id='uncontrolled-native'
                   value={formik.values.country}
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 >
                   <option value={'USA'}>USA</option>
                   <option value={'United Kingdom'}>United Kingdom</option>
@@ -107,8 +137,12 @@ const InformationStep1 = (props) => {
                          placeholder="First Name"
                          onChange={formik.handleChange}
                          value={formik.values.firstName}
+                         onBlur={formik.handleBlur}
                          className={style.container_main_form_container_inputs_input}
               />
+              {formik.touched.firstName && formik.errors.firstName && (
+                <p className={style.error}>{formik.errors.firstName}</p>
+              )}
             </div>
             <div className={style.container_main_form_container_inputs}>
               <TextField id="lastName"
@@ -119,20 +153,34 @@ const InformationStep1 = (props) => {
                          placeholder="Last Name"
                          onChange={formik.handleChange}
                          value={formik.values.lastName}
+                         onBlur={formik.handleBlur}
                          className={style.container_main_form_container_inputs_input}
               />
+              {formik.touched.lastName && formik.errors.lastName && (
+                <p className={style.error}>{formik.errors.lastName}</p>
+              )}
             </div>
             <div className={style.container_main_form_container_inputs}>
-              <TextField id="phone"
-                         label="Phone"
-                         variant="outlined"
-                         type="text"
-                         name="phone"
-                         placeholder="Phone"
-                         onChange={formik.handleChange}
-                         value={formik.values.phone}
-                         className={style.container_main_form_container_inputs_input}
+              <ReactPhoneInput
+                defaultCountry={'us'}
+                component={TextField}
+                id="phone"
+                label="Phone"
+                variant="outlined"
+                type="text"
+                name="phone"
+                placeholder="Phone"
+                onChange={handleSetPhone}
+                value={phoneInput}
+                onBlur={formik.handleBlur}
+                className={style.container_main_form_container_inputs_input}
               />
+              {formik.touched.phone && formik.errors.phone && (
+                <p className={style.error}>{formik.errors.phone}</p>
+              )}
+              {
+                countryInfo !== null && <div className={style.container_main_form_container_inputs_flag}>{countryInfo.flag}</div>
+              }
             </div>
             <div className={style.container_main_form_container_inputs}>
               <TextField id="email"
@@ -143,8 +191,12 @@ const InformationStep1 = (props) => {
                          placeholder="Email"
                          onChange={formik.handleChange}
                          value={formik.values.email}
+                         onBlur={formik.handleBlur}
                          className={style.container_main_form_container_inputs_input}
               />
+              {formik.touched.email && formik.errors.email && (
+                <p className={style.error}>{formik.errors.email}</p>
+              )}
             </div>
             <div className={style.container_main_form_container_inputs}>
               <FormGroup>
