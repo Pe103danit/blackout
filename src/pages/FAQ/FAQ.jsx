1bed17026f4014b12434a71a261f034cbb807d02
Max Kutsulyma <xkutsulymkax@gmail.com>
2023-09-12 19:31:06 +0300

    commit

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -19,7 +19,7 @@ const FAQConfig = [{
     answer: 'Use the DELTA Max Extra Battery Cable to directly connect to Wave.',
     id: '3'
 }, {
-    question: 'Under cooling mode, how long can Wave battery pack, DELTA Max, and DELTA Pro support the operation of Wave?',
+    question: 'How long can Wave battery support the operation of Wave?',
     answer: 'It depends on the battery you choose to use:',
     answerList: ['Wave add-on battery: 3 hours',
         'DELTA Max: 5-6 hours',
@@ -41,7 +41,7 @@ const FAQ = () => {
 
     return (
         <div className={style.faq}>
-            <div zltclassName={style.faq_image}>
+            <div alt='Welcome to EcoFlow Support' className={style.faq_image}>
                 <h3 className={style.faq_title} >Welcome to EcoFlow Support</h3>
                 <h5 className={style.faq_subtitle} >We're here for you</h5>
             </div>
