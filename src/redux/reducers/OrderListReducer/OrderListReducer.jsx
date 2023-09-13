e6ee74078831cc34a05e0b766fc8352365a6c8d8
Maxim Yatsenko <maximkayatsenccko9@gmail.com>
2023-09-14 01:08:36 +0300

    new changessss

--- src/redux/reducers/OrderListReducer/OrderListReducer.jsx
+++ src/redux/reducers/OrderListReducer/OrderListReducer.jsx
@@ -1,19 +1,19 @@
-import types from '../../types/types'
-export const setOrderList = (orderList) => (
-    {
-        type: types.SET_ORDER_LIST,
-        payload: orderList,
-    }
-)
+// import types from '../../types/types'
+// export const setOrderList = (orderList) => (
+//     {
+//         type: types.SET_ORDER_LIST,
+//         payload: orderList,
+//     }
+// )
 
-const initialState = { orderList: [] }
-const OrderListReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case types.SET_ORDER_LIST:
-            return { ...state, orderList: action.payload }
-        default:
-            return state
-    }
-}
+// const initialState = { orderList: [] }
+// const OrderListReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case types.SET_ORDER_LIST:
+//             return { ...state, orderList: action.payload }
+//         default:
+//             return state
+//     }
+// }
 
-export default OrderListReducer
+// export default OrderListReducer
