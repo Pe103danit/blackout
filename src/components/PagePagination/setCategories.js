3b6669a54fe7891b3f9268423b3dcf80f28fca9e
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2023-09-19 18:30:21 +0300

    - some fixes

--- src/components/PagePagination/setCategories.js
+++ src/components/PagePagination/setCategories.js
@@ -3,7 +3,7 @@ export const setCategories = (pathname) => {
     case 'portable_power_stations': return 'Portable Power Stations'
     case 'power_banks': return 'Power Banks'
     case 'generators': return 'Generators'
-    case 'solar_panels': return 'Solar'
+    case 'solar_panels': return 'Solar Panels'
     case 'accessories': return 'Accessories'
     default: return pathname
   }
