9944bceda5573e5bee1c55eeffa4d0e0d6716bb5
Yas-X <yasx80@gmail.com>
2023-09-21 18:51:50 +0300

    Make changes at SelectBar

--- src/components/SelectBar/SelectBar.jsx
+++ src/components/SelectBar/SelectBar.jsx
@@ -1,34 +1,36 @@
 import React, { useState } from 'react'
-import Select from 'react-select';
+// import Select from 'react-select';
 import style from './SelectBar.module.scss';
 
-const options = [
+// const options = [
 
-  { value: '', label: 'Default', isSelected: true },
-  { value: 'sort=-currentPrice', label: 'Decrease Price' },
-  { value: 'sort=+currentPrice', label: 'Increase Price' },
+//   { value: '', label: 'Default', isSelected: true },
+//   { value: 'sort=-currentPrice', label: 'Decrease Price' },
+//   { value: 'sort=+currentPrice', label: 'Increase Price' },
 
-];
+// ];
 
 const SelectBar = () => {
-  const [selectedOption, setSelectedOption] = useState(options[0]);
+  // const [selectedOption, setSelectedOption] = useState(options[0]);
 
-  const handleOptionChange = (selectedOption) => {
-    setSelectedOption(selectedOption)
-  }
+  // const handleOptionChange = (selectedOption) => {
+  //   setSelectedOption(selectedOption)
+  // }
 
   return (
     <div className={style.selectBar}>
       <span>Sort by</span>
-      <Select
-        className={style.selectBar__option}
-        style={{
-          '.css-13cymwt-control ': '#2164FF',
-        }}
+      <select name='select' className={style.selectBar__input}>
+        <option value='' selected>Default</option>
+        <option value='sort=-currentPrice' >Decrease Price</option>
+        <option value='sort=+currentPrice'>Increase Price</option>
+      </select>
+      {/* <Select
+               className={style.selectBar__option}
         value={selectedOption}
         onChange={handleOptionChange}
         options={options}
-      />
+      /> */}
     </div>
   );
 };
