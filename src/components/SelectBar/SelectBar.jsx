c98d9e5ae731a5a6c3e1fa65e0e9176e16ead7a0
Yas-X <yasx80@gmail.com>
2023-09-23 09:00:04 +0300

    Add previous Price to Offers and clored Red actual price - Fix bug Versel 5

--- src/components/SelectBar/SelectBar.jsx
+++ src/components/SelectBar/SelectBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import style from './SelectBar.module.scss';
 
 const SelectBar = () => {
-    return (
+  return (
     <div className={style.selectBar}>
       <span>Sort by</span>
       <select name='select' className={style.selectBar__input}>
-        <option className={style.selectBar__input__option} value='' defaultValue>Default</option>
+        <option className={style.selectBar__input__option} value='' selected>Default</option>
         <option className={style.selectBar__input__option} value='sort=-currentPrice' >From Low to High</option>
         <option className={style.selectBar__input__option} value='sort=+currentPrice'>From High to Low</option>
       </select>
