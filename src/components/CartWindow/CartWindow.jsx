4bbbe61c6fb2c04b8897c5ebbe786d79bff7cd52
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2023-10-04 22:58:13 +0300

    - some fixes

--- src/components/CartWindow/CartWindow.jsx
+++ src/components/CartWindow/CartWindow.jsx
@@ -3,10 +3,11 @@ import { makeShortText } from '../assets/makeShortText'
 import style from '../CartWindow/CartWindow.module.scss'
 const CartWindow = () => {
   const product = useSelector(state => state.ProductReducer?.basketCard)
+  const img = product?.imageUrls[0].replace('/upload/', '/upload/w_90/')
   return (
     <div className={style.win} >
       <h3 className={style.win_text}>PRODUCT WAS ADDED</h3>
-      <img className={style.win_image} src={product?.imageUrls[0]} alt={product?.name} title={product?.name}/>
+      <img className={style.win_image} src={img} alt={product?.name} title={product?.name}/>
       <p>{(product?.name.length > 30)
         ? makeShortText(product?.name)
         : product?.name}</p>
--- src/components/SwiperProductsHome/SwiperProductsHomeContainer.js
+++ src/components/SwiperProductsHome/SwiperProductsHomeContainer.js
@@ -1,14 +1,15 @@
 import { connect } from 'react-redux'
 import { toggleTheme } from '../../redux/reducers/UIStateReducer/UIStateReducer'
 import SwiperProductsHome from './SwiperProductsHome'
+import { getProductsSliderMain } from '../../redux/reducers/ProductReducer/ProductReducer'
 
 const mapStateToProps = (state) => ({
-  ...state.UIStateReducer,
-  products: state.ProductReducer.products
+  ...state.UIStateReducer
 })
 
 const mapDispatchToProps = {
   toggleTheme,
+  getProductsSliderMain
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SwiperProductsHome)
